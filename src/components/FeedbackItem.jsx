4e82abf9c0d049db18a05ff3919bb8b10a27cb50
Brian Nduhiu <briannduhiu@gmail.com>
2022-09-26 19:43:54 +0300

    update

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,5 +1,6 @@
 
 // import {useState} from 'react'
+import Card from "./shared/Card"
 
 function FeedbackItem({item}) {
 
@@ -12,13 +13,19 @@ function FeedbackItem({item}) {
 //     setRating((rating)=>rating+1)
 //   }
   return (
-    <div className="card">
+    // <div className="card">
+        // <div className="num-display">{item.rating}</div>
+        // <div className="text-display">{item.text}</div>
+
+    //     {/* button to change state value using setRating function */}
+    //     {/* <button onClick={handleClick}>Click</button> */}
+    // </div>
+
+    <Card>
         <div className="num-display">{item.rating}</div>
         <div className="text-display">{item.text}</div>
-
-        {/* button to change state value using setRating function */}
-        {/* <button onClick={handleClick}>Click</button> */}
-    </div>
+    </Card>
+    
   )
 }
 

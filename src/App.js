0474aa4e5fc114a3c3751637727e9801f1ba6ac6
Brian Nduhiu <briannduhiu@gmail.com>
2022-09-26 19:30:49 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 
+import { useState } from 'react';
 import Header from "./components/Header";
+// import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 //
 //app component that returns a list of objects if showComments variable is true
@@ -47,14 +51,14 @@ import Header from "./components/Header";
 //     );
 // };
 
-
-
 const App = () => {
+
+    const [feedback, setFeedback] = useState(FeedbackData);
     return (
         <>
             <Header />
             <div className="container">
-                <h1>My App</h1>
+                <FeedbackList feedback={feedback} />
             </div>
         </>
     );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,11 @@
 
-import {useState} from 'react'
+// import {useState} from 'react'
 
-function FeedbackItem() {
-  const [rating,setRating] = useState(7)
-  const [text,setText] = useState("This is an example of a feedback item")
+function FeedbackItem({item}) {
+
+// declare state 
+//   const [rating,setRating] = useState(7)
+//   const [text,setText] = useState("This is an example of a feedback item")
 
   //change state value using SetRating function
 //   const handleClick = ()=>{
@@ -11,8 +13,8 @@ function FeedbackItem() {
 //   }
   return (
     <div className="card">
-        <div className="num-display">{rating}</div>
-        <div className="text-display">{text}</div>
+        <div className="num-display">{item.rating}</div>
+        <div className="text-display">{item.text}</div>
 
         {/* button to change state value using setRating function */}
         {/* <button onClick={handleClick}>Click</button> */}
